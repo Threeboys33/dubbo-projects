17ea4c0c7d5f50a4af696e2210a88eec166f1c04
Threeboys33 <28470938+Threeboys33@users.noreply.github.com>
2019-11-03 17:14:00 +0800

    dubbo-mult-protol-version-registaries

---
+++ dubbo-cluster/src/main/java/dtstack/san/impl/App.java
@@ -0,0 +1,12 @@
+package dtstack.san.impl;
+
+import com.alibaba.dubbo.container.Main;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
--- dubbo-user/src/java/dtstack/san/App.java
+++ dubbo-user/src/java/dtstack/san/App.java
@@ -1,16 +1,25 @@
 package dtstack.san;
 
+import com.alibaba.dubbo.rpc.RpcContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         IHelloService iHelloService = null;
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("dubbo-user.xml");
         iHelloService = (IHelloService) context.getBean("helloService");
-        ResponseObject responseObject = iHelloService.sayHello(new RequestObject().setID("1"));
+//        ResponseObject responseObject = iHelloService.sayHello(new RequestObject().setID("1"));
+
+        iHelloService.sayHello(new RequestObject().setID("1"));
+        Future<ResponseObject> future = RpcContext.getContext().getFuture();
+        System.out.println("异步调用之前");
+        ResponseObject responseObject = future.get();
         System.err.println(responseObject);
 
     }
